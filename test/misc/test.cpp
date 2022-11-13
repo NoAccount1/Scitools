28459c365477c1a3c3c989eb7de10799b1783fff
NoAccount1 <mouamem2edunom@gmail.com>
2022-11-13 16:10:25 +0100

    test: Looking for a way to return multiple variables in one
    The displayMenu function can now return two differents value : one for the selected element and the other one for the f[1-5] key pressed

--- test/misc/test.cpp
+++ test/misc/test.cpp
@@ -3,13 +3,22 @@
 #include <string>
 #include <vector>
 #include <stdarg.h>
+#include <math.h>
 // #include <optional>
 
 using namespace std;
 
+int extract();
+
 int main() {
-  int a = 5;
-  cout << round(a) << endl;
+  int toto = 12345;
+  int len = 5;
+  cout << sizeof(toto) << endl;
+  for (int i = 0; i < pow(10, len);)
+  {
+    i = i ? i*10 : 1;
+    cout << floor(toto/(i*10))*(i*10) << endl;
+  }
 
   return 0;
 }
